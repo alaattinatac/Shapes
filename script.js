d5e18ddfd4d7da291452c63fc1964ccca5f2b949
Alaattin Ataç <alaattinatac@hotmail.com>
2022-01-10 11:34:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function shapeApp(num) {
+
+    console.log(shapesSides[num].shape.toUpperCase())
+
+}
+
+shapeApp(5);
+shapeApp(7);
+shapeApp(9);
---
+++ shapes.js
@@ -0,0 +1,42 @@
+const shapesSides = [
+    {
+        shape: "circle",
+        sides: "1"
+    },
+    {
+        shape: "semi-circle",
+        sides: "2"
+    },
+    {
+        shape: "triangle",
+        sides: "3"
+    },
+    {
+        shape: "square",
+        sides: "4"
+    },
+    {
+        shape: "pentagon",
+        sides: "5"
+    },
+    {
+        shape: "hexagon",
+        sides: "6"
+    },
+    {
+        shape: "heptagon",
+        sides: "7"
+    },
+    {
+        shape: "octagon",
+        sides: "8"
+    },
+    {
+        shape: "nonagon",
+        sides: "9"
+    },
+    {
+        shape: "decagon",
+        sides: "10"
+    },
+]
